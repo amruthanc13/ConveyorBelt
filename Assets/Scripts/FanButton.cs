57a2de96ea241904f24f7fb20519090ef9f5016d
Reshmavasan <reshmavasan14@gmail.com>
2023-02-01 12:22:42 +0100

    Button controller for rotation of fan

---
+++ Assets/Scripts/FanButton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanButton : MonoBehaviour
+{
+    public GameObject fan;
+    public float rotateSpeed = 100;
+    bool rotateStatus = false;
+
+    public void RotateObject()
+    {
+        if(rotateStatus == true)
+        {
+            rotateStatus = false;
+        }
+        else {
+            rotateStatus = true;
+        }
+    }
+
+    void Update()
+    {
+        if(rotateStatus == true)
+        {
+            fan.transform.Rotate(0,0, rotateSpeed * Time.deltaTime);
+        }
+    }
+}

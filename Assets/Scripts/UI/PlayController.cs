c24548947dbb113ebd8d679e5512b0eca13dd17f
amrutha.narendran <amrutha.narendran@st.ovgu.de>
2023-02-20 16:56:14 +0100

    Added refresh button logic

--- Assets/Scripts/UI/PlayController.cs
+++ Assets/Scripts/UI/PlayController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayController : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class PlayController : MonoBehaviour
     public Image image;
     public Sprite playIcon;
     public Sprite pauseIcon;
+    public GameObject refreshButton;
 
     void Start()
     {
@@ -25,6 +27,7 @@ public class PlayController : MonoBehaviour
             image.sprite = pauseIcon;
             Time.timeScale = 1;
             playing = false;
+            refreshButton.SetActive(true);
         }
         else
         {
@@ -32,6 +35,12 @@ public class PlayController : MonoBehaviour
             image.sprite = playIcon;
             Time.timeScale = 0;
             playing = true;
+            refreshButton.SetActive(true);
         }
+    }
+
+    public void refeshProcess()
+    {
+        SceneManager.LoadScene("Process");
     }
 }

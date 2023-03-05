037526e7d3dc8d2ce3d7ead11ea92fb66f57543b
amrutha.narendran <amrutha.narendran@st.ovgu.de>
2023-03-05 15:52:29 +0100

    Added hover script

---
+++ Assets/Scripts/UI/Hover.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    public GameObject Image;
+    public GameObject Tooltip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void onHoverButton()
+    {
+
+            Image.SetActive(false);
+            Tooltip.SetActive(true);
+           
+    }
+
+    public void onLeavingButton()
+    {
+
+        Image.SetActive(true);
+        Tooltip.SetActive(false);
+    }
+}

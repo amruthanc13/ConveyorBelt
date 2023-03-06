3b80e00587d0cf8301292bca2493b904a72ba77f
amrutha.narendran <amrutha.narendran@st.ovgu.de>
2023-03-06 13:20:26 +0100

    Heating block bug fixed

--- Assets/Scripts/UI/Parameters/AirFlow.cs
+++ Assets/Scripts/UI/Parameters/AirFlow.cs
@@ -9,6 +9,7 @@ public class AirFlow : MonoBehaviour
     ToggleGroup toggleGroup;
     [SerializeField] private Slider coCurrentSlider;
     [SerializeField] private Slider counterCurrentSlider;
+
     void Start()
     {
         toggleGroup = GetComponent<ToggleGroup>();
@@ -29,6 +30,9 @@ public class AirFlow : MonoBehaviour
         counterCurrentSlider.value = 0;
         coCurrentSlider.interactable = true;
         coCurrentSlider.value = 100;
+        GameObject.Find("heating_blocks_left").GetComponent<heater>().enabled = true;
+        GameObject.Find("heating_blocks_right").GetComponent<heater>().enabled = false;
+
     }
 
     public void counterCurrentButtonClick()
@@ -37,5 +41,7 @@ public class AirFlow : MonoBehaviour
         counterCurrentSlider.value = 100;
         coCurrentSlider.value = 0;
         coCurrentSlider.interactable = false;
+        GameObject.Find("heating_blocks_left").GetComponent<heater>().enabled = false;
+        GameObject.Find("heating_blocks_right").GetComponent<heater>().enabled = true;
     }
 }
